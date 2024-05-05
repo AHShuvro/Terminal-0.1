e67a3469ca4a1dc16f9d835bc0de6e11c5f49322
A H Shuvro <ahshuvro77777@gmail.com>
2024-05-05 17:07:27 +0600

    Added random UUID dynamicaly

--- src/Components/Local.jsx
+++ src/Components/Local.jsx
@@ -1,13 +1,28 @@
+import { useEffect, useState } from "react";
 
 const Local = () => {
+    const generateUUID = () => {
+        const randomUUID = Math.random().toString(16).slice(2, 6) + ' ' + Math.random().toString(16).slice(2, 6) + ' ' + Math.random().toString(16).slice(2, 6) + ' ' + Math.random().toString(16).slice(2, 6) + ':' + Math.random().toString(16).slice(2, 6)+ ':' + Math.random().toString(16).slice(2, 6);
+        return randomUUID.toUpperCase();
+    };
+
+    const [uuid, setUUID] = useState('');
+
+    useEffect(() => {
+        setUUID(generateUUID());
+    }, []);
+
+    console.log(uuid);
+
     return (
         <div className="font-medium tracking-custom">
             <p className="text-[#CD7638]">Welcome to ShellFolio v1992.05 !</p>
-            <p className="text-[#CD7638]">Session based UUID: <span className="text-[#FCE26E]">233d e0aa a406 79a9 : a540  : 1079</span></p>
+            <p className="text-[#CD7638]">Session based UUID: <span className="text-[#FCE26E]">{uuid}</span></p>
             <p className="text-[#CD7638]">Type <span className="text-[#7FFFD4]">"help"</span> to see available commands.</ p>
             <p className="text-[#FCE26E]">All Commands are case insensitive.</p>
         </div>
     );
 };
+
 
 export default Local;
