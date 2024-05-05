d8bc7c5b28262cd62182e1ff53e88a0bca770ff7
A H Shuvro <ahshuvro77777@gmail.com>
2024-05-05 09:16:17 +0600

    Fix some responsive issues

--- src/App.jsx
+++ src/App.jsx
@@ -99,7 +99,7 @@ function App() {
             <Link to={'/blank'} ><button className="text-[#64F105] hover:text-[#CD7638] text-xl">⊖</button></Link>
             <Link to={'/close'} ><button className="text-[#CD7638] hover:text-[#F5084F] text-xl">⊗</button></Link>
           </div>
-          <div className="pl-8 pr-8 md:pr-32 h-[30rem] rounded-xl overflow-y-auto" style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }}>
+          <div className="pl-8 pr-8 md:pr-32 h-[30rem] rounded-xl overflow-y-auto mx-3" style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }}>
             <Local />
             <div>
               {output.map((item, idx) => (
