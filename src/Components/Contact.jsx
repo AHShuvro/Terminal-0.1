b2cb01fb29046a5625953d41bb02d60b2d3c90e8
A H Shuvro <ahshuvro77777@gmail.com>
2024-05-29 19:48:28 +0600

    update some informations

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -3,7 +3,7 @@ const Contact = () => {
     return (
         <div className='text-wrap font-medium tracking-custom py-2'>
             <p className='text-[#FCE26E] tracking-custom'>In case you need me, drop a message via <span className="text-[#00FFFF]">'ahshuvro77777@gmail.com'</span>. I assure you I'll get back to you as soon as I can.
-                Are you social? Below are both clickable <span className="text-[#00FFFF]">links</span> & <span className="text-[#00FFFF]">usable commands</span>.</p>
+                Are you social? Below are clickable <span className="text-[#00FFFF]">links</span>.</p>
             <h3 className="text-[#64F105]"><span className="text-[#00FFFF]">⇒ </span><a href="https://www.linkedin.com/in/ahshuvro77777/" target="_blank">LinkedIn</a></h3>
             <h3 className="text-[#64F105]"><span className="text-[#00FFFF]">⇒ </span><a href="https://github.com/AHShuvro" target="_blank">GitHub</a></h3>
             <h3 className="text-[#64F105]"><span className="text-[#00FFFF]">⇒ </span><a href="https://www.facebook.com/ahshuvro7777" target="_blank">Facebook</a></h3>
