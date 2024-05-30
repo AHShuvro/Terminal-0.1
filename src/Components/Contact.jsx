8f2988691787d27e17b98607dcbde64f37bd251c
A H Shuvro <ahshuvro77777@gmail.com>
2024-05-30 22:26:32 +0600

    change some designs

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,8 +2,7 @@
 const Contact = () => {
     return (
         <div className=' font-medium tracking-custom py-2'>
-            <p className='text-[#DAA520] dark:text-[#FCE26E] tracking-custom'>In case you need me, drop a message via <span className="text-[#32CD32] dark:text-[#6EF500]">ahshuvro77777@gmail.com</span>. I assure you I'll get back to you as soon as I can.
-                Are you social? Below are clickable <span className="text-[#32CD32] dark:text-[#6EF500]">links</span>.</p>
+            <p className='text-[#DAA520] dark:text-[#FCE26E] tracking-custom'>In case you need me, drop a message via <span className="text-[#32CD32] dark:text-[#6EF500]">ahshuvro77777@gmail.com</span>. I assure you I'll get back to you as soon as I can. Are you social? Below are clickable <span className="text-[#32CD32] dark:text-[#6EF500]">links</span>.</p>
             <h3 className="text-[#64F105]"><span className="text-[#32CD32] dark:text-[#6EF500]">⇒ </span><a className="text-[#FF6347] dark:text-[#FFA500] hover:text-[#4682B4] dark:hover:text-[#87CEEB]" href="https://www.linkedin.com/in/ahshuvro77777/" target="_blank">LinkedIn</a></h3>
             <h3 className="text-[#64F105]"><span className="text-[#32CD32] dark:text-[#6EF500]">⇒ </span><a className="text-[#FF6347] dark:text-[#FFA500] hover:text-[#4682B4] dark:hover:text-[#87CEEB]" href="https://github.com/AHShuvro" target="_blank">GitHub</a></h3>
             <h3 className="text-[#64F105]"><span className="text-[#32CD32] dark:text-[#6EF500]">⇒ </span><a className="text-[#FF6347] dark:text-[#FFA500] hover:text-[#4682B4] dark:hover:text-[#87CEEB]" href="https://www.facebook.com/ahshuvro7777" target="_blank">Facebook</a></h3>
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -4,7 +4,7 @@ const Projects = () => {
     return (
         <div className=' font-medium tracking-custom py-2'>
             <h2 className="text-[#DAA520] dark:text-[#FCE26E]">My Projects</h2>
-            <p className='text-[#DAA520] dark:text-[#FCE26E]'>Check out some of my projects below:</p>
+            <p className='text-[#DAA520] dark:text-[#FCE26E]'>Check out some of my projects below are clickable <span className="text-[#32CD32] dark:text-[#6EF500]">links</span>.</p>
             <h3 className="text-[#228B22] dark:text-[#64F105]"><span className="text-[#FFA500]">⇒ </span><a className="text-[#FF6347] dark:text-[#FFA500] hover:text-[#4682B4] dark:hover:text-[#87CEEB]" href="https://clothing-clone-site.vercel.app/" target="_blank">E-Commerce 1</a></h3>
             <h3 className="text-[#228B22] dark:text-[#64F105]"><span className="text-[#FFA500]">⇒ </span><a className="text-[#FF6347] dark:text-[#FFA500] hover:text-[#4682B4] dark:hover:text-[#87CEEB]" href="https://brand-shop-fawn.vercel.app/" target="_blank">E-Commerce 2</a></h3>
             <h3 className="text-[#228B22] dark:text-[#64F105]"><span className="text-[#FFA500]">⇒ </span><a className="text-[#FF6347] dark:text-[#FFA500] hover:text-[#4682B4] dark:hover:text-[#87CEEB]" href="https://ahsui.vercel.app/" target="_blank">Components library</a></h3>
