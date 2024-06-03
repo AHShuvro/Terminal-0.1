12eb99be9c55e4dc6fa8bfad59e72f7b3246f2f2
A H Shuvro <ahshuvro77777@gmail.com>
2024-06-03 15:32:24 +0600

    fix some issues

--- src/Components/Blank.jsx
+++ src/Components/Blank.jsx
@@ -12,12 +12,9 @@ const Blank = () => {
         }
     }, []);
 
-    const resetTheme = () => {
-        localStorage.setItem('theme', 'dark');
-    }
     return (
         <div className={`${theme} flex justify-center items-center h-screen bg-[#FAFAFA] dark:bg-[#011627]`}>
-            <Link to={'/'}><p onClick={() => resetTheme()} className="text-[#00CED1] hover:text-[#4682B4] dark:text-[#6EF500] dark:hover:text-[#FEC303] italic font-semibold tracking-custom">Start</p></Link>
+            <Link to={'/'}><p className="text-[#00CED1] hover:text-[#4682B4] dark:text-[#6EF500] dark:hover:text-[#FEC303] italic font-semibold tracking-custom">Start</p></Link>
         </div>
     );
 };
