ea594618453fd52767c4fc03a3b8e77bfd430530
A H Shuvro <ahshuvro77777@gmail.com>
2024-06-12 14:31:35 +0600

    Change some design and add some new information

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -3,11 +3,11 @@ const Skills = () => {
     return (
         <div className='text-wrap font-medium tracking-custom py-2'>
             <p className="text-[#DAA520] dark:text-[#ECAE32]">I can assist you with</p>
-            <p className="text-[#DAA520] dark:text-[#ECAE32]"><span className="text-[#B22222] dark:text-[#E70347]">★</span> Proficient in HTML, CSS, Tailwind, JavaScript</p>
-            <p className="text-[#DAA520] dark:text-[#ECAE32]"><span className="text-[#B22222] dark:text-[#E70347]">★</span> Experience with modern frontend framework React</p>
-            <p className="text-[#DAA520] dark:text-[#ECAE32]"><span className="text-[#B22222] dark:text-[#E70347]">★</span> Experience with RESTful APIs</p>
-            <p className="text-[#DAA520] dark:text-[#ECAE32]"><span className="text-[#B22222] dark:text-[#E70347]">★</span> Javascript, Java, C/C++</p>
-            <p className="text-[#DAA520] dark:text-[#ECAE32]"><span className="text-[#B22222] dark:text-[#E70347]">★</span> Git, GitHub</p>
+            <p className="text-[#DAA520] dark:text-[#ECAE32]"><span className="text-[#B22222] dark:text-[#E70347]">★</span> Proficient in <span className="text-[#4682B4] dark:text-[#7FFFD4]">HTML</span>, <span className="text-[#4682B4] dark:text-[#7FFFD4]">CSS</span>, <span className="text-[#4682B4] dark:text-[#7FFFD4]">Tailwind</span>, <span className="text-[#4682B4] dark:text-[#7FFFD4]">JavaScript</span></p>
+            <p className="text-[#DAA520] dark:text-[#ECAE32]"><span className="text-[#B22222] dark:text-[#E70347]">★</span> Experience with modern frontend library or framework: <span className="text-[#4682B4] dark:text-[#7FFFD4]">React</span> and <span className="text-[#4682B4] dark:text-[#7FFFD4]">Next.js</span> </p>
+            <p className="text-[#DAA520] dark:text-[#ECAE32]"><span className="text-[#B22222] dark:text-[#E70347]">★</span> Experience with using <span className="text-[#4682B4] dark:text-[#7FFFD4]">API</span></p>
+            <p className="text-[#DAA520] dark:text-[#ECAE32]"><span className="text-[#B22222] dark:text-[#E70347]">★</span>Familiar with <span className="text-[#4682B4] dark:text-[#7FFFD4]">JAVA</span>,<span className="text-[#4682B4] dark:text-[#7FFFD4]"> C/C++</span></p>
+            <p className="text-[#DAA520] dark:text-[#ECAE32]"><span className="text-[#B22222] dark:text-[#E70347]">★</span> Version control with  <span className="text-[#4682B4] dark:text-[#7FFFD4]">Git</span>,  <span className="text-[#4682B4] dark:text-[#7FFFD4]">GitHub</span></p>
             <p className='text-[#228B22] dark:text-[#64F105]'><span className="text-[#CD853F] dark:text-[#ECAE32]">⇒</span> Hello, I'm A H Shuvro, a frontend web developer seeking job opportunities. Thank you!</p>
         </div>
 
