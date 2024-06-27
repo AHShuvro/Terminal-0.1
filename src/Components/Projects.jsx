25bafcc6d6f6a3885a2d4eadf3ea0acaa33a0cf5
A H Shuvro <ahshuvro77777@gmail.com>
2024-06-27 15:54:47 +0600

    Change some design

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -4,7 +4,7 @@ const Projects = () => {
     return (
         <div className=' font-medium tracking-custom py-2'>
             <h2 className="text-[#DAA520] dark:text-[#FCE26E]">My Projects</h2>
-            <p className='text-[#DAA520] dark:text-[#FCE26E]'>Check out some of my projects below are clickable <span className="text-[#32CD32] dark:text-[#6EF500]">links</span>.</p>
+            <p className='text-[#DAA520] dark:text-[#FCE26E]'>Check out some of my projects below are <span className="text-[#32CD32] dark:text-[#6EF500]">clickable links</span>.</p>
             <h3 className="text-[#228B22] dark:text-[#64F105]"><span className="text-[#FFA500]">⇒ </span><a className="text-[#FF6347] dark:text-[#FFA500] hover:text-[#4682B4] dark:hover:text-[#87CEEB]" href="https://ah-shuvro-portfolio.vercel.app/" target="_blank">Portfolio</a></h3>
             <h3 className="text-[#228B22] dark:text-[#64F105]"><span className="text-[#FFA500]">⇒ </span><a className="text-[#FF6347] dark:text-[#FFA500] hover:text-[#4682B4] dark:hover:text-[#87CEEB]" href="https://ahsui.vercel.app/" target="_blank">Components library</a></h3>
             <h3 className="text-[#228B22] dark:text-[#64F105]"><span className="text-[#FFA500]">⇒ </span><a className="text-[#FF6347] dark:text-[#FFA500] hover:text-[#4682B4] dark:hover:text-[#87CEEB]" href="https://clothing-clone-site.vercel.app/" target="_blank">E-Commerce 1</a></h3>
